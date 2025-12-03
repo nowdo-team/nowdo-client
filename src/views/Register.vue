d2e1ce5c39b6184815e2446c9ebcc52c42302b63
PR0623-pixel <fksfksfn623@gmail.com>
2025-12-03 14:15:45 +0900

    Register.vue 수정

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,6 +2,7 @@
   <h2>회원가입</h2>
   <input v-model="email" placeholder="이메일" />
   <input v-model="password" placeholder="비밀번호" />
+  <input v-model="nickname" placeholder="닉네임" />
   <button @click="doRegister">가입</button>
 </template>
 
@@ -12,10 +13,11 @@ import { useRouter } from 'vue-router'
 
 const email = ref('')
 const password = ref('')
+const nickname = ref('')
 const router = useRouter()
 
 const doRegister = async () => {
-  await register({ email: email.value, password: password.value })
+  await register({ email: email.value, password: password.value, nickname: nickname.value })
   router.push('/')
 }
 </script>
